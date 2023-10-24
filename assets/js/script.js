d4c28aad236952e364876c9cf79068d8d572cbd2
hari babu <thughari3@gmail.com>
2023-10-24 15:20:36 +0530

    autoplay

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,71 +11,71 @@
 const musicData = [
   {backgroundImage: "./assets/images/Oy!.jpg",title: "Oy! Oy!",album: "Oy",year: 2009,artist: "",musicPath: "./assets/music/01 - Oy! Oy! - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Yedhane Koiyyake",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/01 - Yedhane Koiyyake - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Yedhane Koiyyake",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "Unknown",musicPath: "./assets/music/01 - Yedhane Koiyyake - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Naalona Pongenu",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/02 - Naalona Pongenu - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Naalona Pongenu",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "Unknown",musicPath: "./assets/music/02 - Naalona Pongenu - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/Oy!.jpg",title: "Saradaga",album: "Oy",year: 2009,artist: "No Spirit",musicPath: "./assets/music/02 - Saradaga - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/Oy!.jpg",title: "Saradaga",album: "Oy",year: 2009,artist: "Unknown",musicPath: "./assets/music/02 - Saradaga - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/Oy!.jpg",title: "Waiting For You",album: "Oy",year: 2009,artist: "No Spirit",musicPath: "./assets/music/03 - Waiting For You - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/Oy!.jpg",title: "Waiting For You",album: "Oy",year: 2009,artist: "Unknown",musicPath: "./assets/music/03 - Waiting For You - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Yegasi Yegasi",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/03 - Yegasi Yegasi - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Yegasi Yegasi",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "Unknown",musicPath: "./assets/music/03 - Yegasi Yegasi - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/Oy!.jpg",title: "Anukoledenadu",album: "Oy",year: 2009,artist: "No Spirit",musicPath: "./assets/music/04 - Anukoledenadu - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/Oy!.jpg",title: "Anukoledenadu",album: "Oy",year: 2009,artist: "Unknown",musicPath: "./assets/music/04 - Anukoledenadu - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Monna Kanipinchavu",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/04 - Monna Kanipinchavu - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Monna Kanipinchavu",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "Unknown",musicPath: "./assets/music/04 - Monna Kanipinchavu - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "O Shanthi Shanthi",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/05 - O Shanthi Shanthi - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "O Shanthi Shanthi",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "Unknown",musicPath: "./assets/music/05 - O Shanthi Shanthi - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/Oy!.jpg",title: "Povodhe Prema",album: "Oy",year: 2009,artist: "No Spirit",musicPath: "./assets/music/05 - Povodhe Prema - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/Oy!.jpg",title: "Povodhe Prema",album: "Oy",year: 2009,artist: "Unknown",musicPath: "./assets/music/05 - Povodhe Prema - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Adhey Nannu",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/06 - Adhey Nannu - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Adhey Nannu",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "Unknown",musicPath: "./assets/music/06 - Adhey Nannu - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/Oy!.jpg",title: "Seheri",album: "Oy",year: 2009,artist: "No Spirit",musicPath: "./assets/music/06 - Seheri - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/Oy!.jpg",title: "Seheri",album: "Oy",year: 2009,artist: "Unknown",musicPath: "./assets/music/06 - Seheri - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Nidhare Kala",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/07 - Nidhare Kala - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Nidhare Kala",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "Unknown",musicPath: "./assets/music/07 - Nidhare Kala - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/Orange.jpg",title: "Ola Olaala Ala",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/1-Ola Olaala Ala-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "Ola Olaala Ala",album: "Orange",year: 2010,artist: "Unknown",musicPath: "./assets/music/1-Ola Olaala Ala-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Orange.jpg",title: "Chilipiga",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/2-Chilipiga-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "Chilipiga",album: "Orange",year: 2010,artist: "Unknown",musicPath: "./assets/music/2-Chilipiga-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Orange.jpg",title: "Nenu Nuvvantu",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/3-Nenu Nuvvantu-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "Nenu Nuvvantu",album: "Orange",year: 2010,artist: "Unknown",musicPath: "./assets/music/3-Nenu Nuvvantu-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Orange.jpg",title: "Hello Rammante",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/4-Hello Rammante-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "Hello Rammante",album: "Orange",year: 2010,artist: "Unknown",musicPath: "./assets/music/4-Hello Rammante-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Orange.jpg",title: "O Range",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/5- O Range-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "O Range",album: "Orange",year: 2010,artist: "Unknown",musicPath: "./assets/music/5- O Range-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Orange.jpg",title: "Rooba Rooba",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/6-Rooba Rooba-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "Rooba Rooba",album: "Orange",year: 2010,artist: "Unknown",musicPath: "./assets/music/6-Rooba Rooba-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Murari.jpg",title: "Alanati Ramachandrudu",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Alanati Ramachandrudu-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Alanati Ramachandrudu",album: "Murari",year: 2001,artist: "Unknown",musicPath: "./assets/music/Alanati Ramachandrudu-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Murari.jpg",title: "Andaanike Aadanive",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Andaanike Aadanive-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Andaanike Aadanive",album: "Murari",year: 2001,artist: "Unknown",musicPath: "./assets/music/Andaanike Aadanive-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Murari.jpg",title: "Bangaru Kalla",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Bangaru Kalla-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Bangaru Kalla",album: "Murari",year: 2001,artist: "Unknown",musicPath: "./assets/music/Bangaru Kalla-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Murari.jpg",title: "Bhama Bhama",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Bhama Bhama-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Bhama Bhama",album: "Murari",year: 2001,artist: "Unknown",musicPath: "./assets/music/Bhama Bhama-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Jalsa.jpg",title: "Chalore Chalore Chal (Telugu)",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/Chalore Chalore Chal (Telugu)-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "Chalore Chalore Chal (Telugu)",album: "Jalsa",year: 2008,artist: "Unknown",musicPath: "./assets/music/Chalore Chalore Chal (Telugu)-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Murari.jpg",title: "Cheppamma Cheppamma",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Cheppamma Cheppamma-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Cheppamma Cheppamma",album: "Murari",year: 2001,artist: "Unknown",musicPath: "./assets/music/Cheppamma Cheppamma-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Murari.jpg",title: "Dum Dum Dum",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Dum Dum Dum-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Dum Dum Dum",album: "Murari",year: 2001,artist: "Unknown",musicPath: "./assets/music/Dum Dum Dum-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Murari.jpg",title: "Ekkada Ekkada",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Ekkada Ekkada-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Ekkada Ekkada",album: "Murari",year: 2001,artist: "Unknown",musicPath: "./assets/music/Ekkada Ekkada-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Jalsa.jpg",title: "Gaallo Thelinattunde",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/Gaallo Thelinattunde-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "Gaallo Thelinattunde",album: "Jalsa",year: 2008,artist: "Unknown",musicPath: "./assets/music/Gaallo Thelinattunde-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Jalsa.jpg",title: "Jalsa",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/Jalsa-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "Jalsa",album: "Jalsa",year: 2008,artist: "Unknown",musicPath: "./assets/music/Jalsa-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Jalsa.jpg",title: "Jennifer Lopez",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/Jennifer Lopez-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "Jennifer Lopez",album: "Jalsa",year: 2008,artist: "Unknown",musicPath: "./assets/music/Jennifer Lopez-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Jalsa.jpg",title: "My Heart Is Beating (Remix)",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/My Heart Is Beating (Remix)-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "My Heart Is Beating (Remix)",album: "Jalsa",year: 2008,artist: "Unknown",musicPath: "./assets/music/My Heart Is Beating (Remix)-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/Jalsa.jpg",title: "My Heart Is Beating",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/My Heart Is Beating-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "My Heart Is Beating",album: "Jalsa",year: 2008,artist: "Unknown",musicPath: "./assets/music/My Heart Is Beating-SenSongsMp3.Co.mp3",
 },
 {backgroundImage: "./assets/images/Divide.jpeg",title: "Perfect",album: "Single",year: 2017,artist: "Ed Sheeran",musicPath: "./assets/music/Perfect(Mr-Jatt1.com).mp3",
 },
-{backgroundImage: "./assets/images/Jalsa.jpg",title: "You & I",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/You & I-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "You & I",album: "Jalsa",year: 2008,artist: "Unknown",musicPath: "./assets/music/You & I-SenSongsMp3.Co.mp3",
 },
 {backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Annhiliate",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Annihilate.mp3",
 },
@@ -104,6 +104,10 @@ const musicData = [
 {backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Nas Morales",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Nas-Morales.mp3",
 },
 {backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Mona Lisa",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Dominic Fike",musicPath: "./assets/music/Dominic Fike - Mona Lisa (Official Audio) (320 kbps).mp3",
+},
+{backgroundImage: "./assets/images/Into-the-spiderverse.webp",title: "Sunflower",album: "Spiderman-Into-the-spiderverse",year: 2018,artist: "Post Malone",musicPath: "./assets/music/Post-Malone-Sunflower-Spider-Man-Into-the-Spider-Verse.mp3",
+},
+{backgroundImage: "./assets/images/Into-the-spiderverse.webp",title: "Whats Up Danger Spider",album: "Spiderman-Into-the-spiderverse",year: 2018,artist: "Dominic Fike",musicPath: "./assets/music/Whats_Up_Danger_Spider-Man_Into_the_Spider-Verse.mp3",
 },
 ];
 
@@ -222,7 +226,7 @@ const playerYear = document.querySelector("[data-year]");
 const playerArtist = document.querySelector("[data-artist]");
 
 const audioSource = new Audio(musicData[currentMusic].musicPath);
-audioSource.autoplay = true; 
+
 const changePlayerInfo = function () {
   playerBanner.src = musicData[currentMusic].backgroundImage;
   playerBanner.setAttribute("alt", `${musicData[currentMusic].title} Album Poster`);
@@ -339,6 +343,16 @@ playerSeekRange.addEventListener("input", seek);
  * END MUSIC
  */
 
+// const isMusicEnd = function () {
+//   if (audioSource.ended) {
+//     playBtn.classList.remove("active");
+//     audioSource.currentTime = 0;
+//     playerSeekRange.value = audioSource.currentTime;
+//     playerRunningTime.textContent = getTimecode(audioSource.currentTime);
+//     updateRangeFill();
+//   }
+// }
+
 const isMusicEnd = function () {
   if (audioSource.ended) {
     playBtn.classList.remove("active");
@@ -346,8 +360,19 @@ const isMusicEnd = function () {
     playerSeekRange.value = audioSource.currentTime;
     playerRunningTime.textContent = getTimecode(audioSource.currentTime);
     updateRangeFill();
+
+    // Skip to the next song
+    lastPlayedMusic = currentMusic;
+    if (isShuffled) {
+      shuffleMusic();
+    } else {
+      currentMusic >= musicData.length - 1 ? (currentMusic = 0) : currentMusic++;
+    }
+    changePlayerInfo();
+    changePlaylistItem();
   }
-}
+};
+
 
 
 

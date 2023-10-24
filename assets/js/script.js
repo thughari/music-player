6277403660b50405529845cb1887fd4761d78bd0
hari babu <thughari3@gmail.com>
2023-10-24 14:30:58 +0530

    added songs and cover

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,76 +9,117 @@
 
 
 const musicData = [
-  {backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "01 - Oy! Oy! - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/01 - Oy! Oy! - SenSongsMp3.co.mp3",
+  {backgroundImage: "./assets/images/Oy!.jpg",title: "Oy! Oy!",album: "Oy",year: 2009,artist: "",musicPath: "./assets/music/01 - Oy! Oy! - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "01 - Yedhane Koiyyake - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/01 - Yedhane Koiyyake - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Yedhane Koiyyake",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/01 - Yedhane Koiyyake - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "02 - Naalona Pongenu - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/02 - Naalona Pongenu - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Naalona Pongenu",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/02 - Naalona Pongenu - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "02 - Saradaga - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/02 - Saradaga - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/Oy!.jpg",title: "Saradaga",album: "Oy",year: 2009,artist: "No Spirit",musicPath: "./assets/music/02 - Saradaga - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "03 - Waiting For You - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/03 - Waiting For You - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/Oy!.jpg",title: "Waiting For You",album: "Oy",year: 2009,artist: "No Spirit",musicPath: "./assets/music/03 - Waiting For You - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "03 - Yegasi Yegasi - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/03 - Yegasi Yegasi - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Yegasi Yegasi",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/03 - Yegasi Yegasi - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "04 - Anukoledenadu - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/04 - Anukoledenadu - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/Oy!.jpg",title: "Anukoledenadu",album: "Oy",year: 2009,artist: "No Spirit",musicPath: "./assets/music/04 - Anukoledenadu - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "04 - Monna Kanipinchavu - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/04 - Monna Kanipinchavu - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Monna Kanipinchavu",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/04 - Monna Kanipinchavu - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "05 - O Shanthi Shanthi - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/05 - O Shanthi Shanthi - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "O Shanthi Shanthi",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/05 - O Shanthi Shanthi - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "05 - Povodhe Prema - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/05 - Povodhe Prema - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/Oy!.jpg",title: "Povodhe Prema",album: "Oy",year: 2009,artist: "No Spirit",musicPath: "./assets/music/05 - Povodhe Prema - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "06 - Adhey Nannu - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/06 - Adhey Nannu - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Adhey Nannu",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/06 - Adhey Nannu - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "06 - Seheri - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/06 - Seheri - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/Oy!.jpg",title: "Seheri",album: "Oy",year: 2009,artist: "No Spirit",musicPath: "./assets/music/06 - Seheri - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "07 - Nidhare Kala - SenSongsMp3.co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/07 - Nidhare Kala - SenSongsMp3.co.mp3",
+{backgroundImage: "./assets/images/surya-s-o-krishnan.avif",title: "Nidhare Kala",album: "SURYA SON OF KRISHNAN",year: 2008,artist: "No Spirit",musicPath: "./assets/music/07 - Nidhare Kala - SenSongsMp3.co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "1-Ola Olaala Ala-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/1-Ola Olaala Ala-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "Ola Olaala Ala",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/1-Ola Olaala Ala-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "2-Chilipiga-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/2-Chilipiga-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "Chilipiga",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/2-Chilipiga-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "3-Nenu Nuvvantu-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/3-Nenu Nuvvantu-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "Nenu Nuvvantu",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/3-Nenu Nuvvantu-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "4-Hello Rammante-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/4-Hello Rammante-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "Hello Rammante",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/4-Hello Rammante-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "5- O Range-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/5- O Range-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "O Range",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/5- O Range-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "6-Rooba Rooba-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/6-Rooba Rooba-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Orange.jpg",title: "Rooba Rooba",album: "Orange",year: 2010,artist: "No Spirit",musicPath: "./assets/music/6-Rooba Rooba-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Alanati Ramachandrudu-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Alanati Ramachandrudu-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Alanati Ramachandrudu",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Alanati Ramachandrudu-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Andaanike Aadanive-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Andaanike Aadanive-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Andaanike Aadanive",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Andaanike Aadanive-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Bangaru Kalla-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Bangaru Kalla-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Bangaru Kalla",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Bangaru Kalla-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Bhama Bhama-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Bhama Bhama-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Bhama Bhama",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Bhama Bhama-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Chalore Chalore Chal (Telugu)-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Chalore Chalore Chal (Telugu)-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "Chalore Chalore Chal (Telugu)",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/Chalore Chalore Chal (Telugu)-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Cheppamma Cheppamma-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Cheppamma Cheppamma-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Cheppamma Cheppamma",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Cheppamma Cheppamma-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Dum Dum Dum-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Dum Dum Dum-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Dum Dum Dum",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Dum Dum Dum-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Ekkada Ekkada-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Ekkada Ekkada-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Murari.jpg",title: "Ekkada Ekkada",album: "Murari",year: 2001,artist: "No Spirit",musicPath: "./assets/music/Ekkada Ekkada-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Gaallo Thelinattunde-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Gaallo Thelinattunde-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "Gaallo Thelinattunde",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/Gaallo Thelinattunde-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Jalsa-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Jalsa-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "Jalsa",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/Jalsa-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Jennifer Lopez-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Jennifer Lopez-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "Jennifer Lopez",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/Jennifer Lopez-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "My Heart Is Beating (Remix)-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/My Heart Is Beating (Remix)-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "My Heart Is Beating (Remix)",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/My Heart Is Beating (Remix)-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "My Heart Is Beating-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/My Heart Is Beating-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "My Heart Is Beating",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/My Heart Is Beating-SenSongsMp3.Co.mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "Perfect(Mr-Jatt1.com).mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/Perfect(Mr-Jatt1.com).mp3",
+{backgroundImage: "./assets/images/Divide.jpeg",title: "Perfect",album: "Single",year: 2017,artist: "Ed Sheeran",musicPath: "./assets/music/Perfect(Mr-Jatt1.com).mp3",
 },
-{backgroundImage: "./assets/images/",posterUrl: "./assets/images/",title: "You & I-SenSongsMp3.Co.mp3",album: "No Spirit",year: 2022,artist: "No Spirit",musicPath: "./assets/music/You & I-SenSongsMp3.Co.mp3",
+{backgroundImage: "./assets/images/Jalsa.jpg",title: "You & I",album: "Jalsa",year: 2008,artist: "No Spirit",musicPath: "./assets/music/You & I-SenSongsMp3.Co.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Annhiliate",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Annihilate.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Am I Dreaming",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Am-I-Dreaming.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "All The Way Live",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-All-The-Way-Live.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Danger-Spider",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Danger-Spider.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Hummingbird",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Hummingbird.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Calling",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Calling.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Silk and Cologne Spider Verse Remix",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Silk-and-Cologne-Spider-Verse-Remix.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Link Up",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Link-Up-Spider-Verse-Remix.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Self-Love",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Self-Love.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Home",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Home.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Nonviolent Communication",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Nonviolent-Communication.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Givin Up Not The One",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Givin-Up-Not-The-One.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Nas Morales",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Metro Boomin",musicPath: "./assets/music/Metro-Boomin-Nas-Morales.mp3",
+},
+{backgroundImage: "./assets/images/Across-the-spiderverse.jpg",title: "Mona Lisa",album: "Spiderman-Across-the-spiderverse",year: 2023,artist: "Dominic Fike",musicPath: "./assets/music/Dominic Fike - Mona Lisa (Official Audio) (320 kbps).mp3",
 },
 ];
 
+const sortedMusicData = musicData.sort((a, b) => {
+  const albumA = a.album.toUpperCase();
+  const albumB = b.album.toUpperCase();
+
+  if (albumA < albumB) {
+    return -1;
+  }
+  if (albumA > albumB) {
+    return 1;
+  }
+
+  return 0;
+});
 
 
 /**
@@ -105,8 +146,12 @@ for (let i = 0, len = musicData.length; i < len; i++) {
   playlist.innerHTML += `
   <li>
     <button class="music-item ${i === 0 ? "playing" : ""}" data-playlist-toggler data-playlist-item="${i}">
-      <img src="${musicData[i].posterUrl}" width="800" height="800" alt="${musicData[i].title} Album Poster"
+      <img src="${musicData[i].backgroundImage}" width="800" height="800" alt="${musicData[i].title} Album Poster"
         class="img-cover">
+        <div class="album-container">
+    <p class="album-name">${musicData[i].title}</p>
+</div>
+
 
       <div class="item-icon">
         <span class="material-symbols-rounded">equalizer</span>
@@ -177,9 +222,9 @@ const playerYear = document.querySelector("[data-year]");
 const playerArtist = document.querySelector("[data-artist]");
 
 const audioSource = new Audio(musicData[currentMusic].musicPath);
-
+audioSource.autoplay = true; 
 const changePlayerInfo = function () {
-  playerBanner.src = musicData[currentMusic].posterUrl;
+  playerBanner.src = musicData[currentMusic].backgroundImage;
   playerBanner.setAttribute("alt", `${musicData[currentMusic].title} Album Poster`);
   document.body.style.backgroundImage = `url(${musicData[currentMusic].backgroundImage})`;
   playerTitle.textContent = musicData[currentMusic].title;
